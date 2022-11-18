9e00ca2e5927dda4cf52f79029cb2d9fe0d32fe8
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-11-18 12:45:47 +0400

    Add files via upload

--- src/Employee.js
+++ src/Employee.js
@@ -1,43 +1,60 @@
 import React, { Component } from "react";
-import "./Employee.css"
+import "./Employee.css";
 
 export default class Employee extends Component {
   state = {
     employes: [
-      { id: 1, name: "Frodo Baggins", department: "Management", role: "CEO" },
-      { id: 2, name: "Samwise Gamgee", department: "Management", role: "CTO" },
+      {
+        id: 1,
+        name: "Frodo Baggins",
+        department: "Management",
+        role: "CEO",
+        alive: true,
+      },
+      {
+        id: 2,
+        name: "Samwise Gamgee",
+        department: "Management",
+        role: "CTO",
+        alive: false,
+      },
       {
         id: 3,
         name: "Gandalf the Gray",
         department: "Recruitment",
         role: "Lead-recruiter",
+        alive: true,
       },
       {
         id: 4,
         name: "Aragorn",
         department: "Security",
         role: "Security officer",
+        alive: false,
       },
       {
         id: 5,
         name: "Legolas",
         department: "Management",
         role: "Office manager",
+        alive: true,
       },
     ],
   };
   render() {
     return (
       <div className="common">
-        {this.state.employes.map((user) => (
-          <div className="child">
-            <p>Employee id: {user.id}</p>
-            <p>Employee name: {user.name}</p>
-            <p>Employee department: {user.department}</p>
-            <p>Employee role: {user.role}</p>
-          <vr/>
-          </div>
-        ))}
+        {this.state.employes
+          .filter((user) => user.alive)
+          .map((user) => (
+            <div className="child" key={user.id}>
+              <p>Employee id: {user.id}</p>
+              <p>Employee name: {user.name}</p>
+              <p>Employee department: {user.department}</p>
+              <p>Employee role: {user.role}</p>
+              <p>Is Employee alive: {user.alive ? "yes" : "not"}</p>
+            </div>
+          ))}
       </div>
     );
   }
